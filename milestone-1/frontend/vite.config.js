617aaae16d0a3f05f967ea7a7228ec781541aa5f
JSiggelkow <jonas.siggelkow@unitybox.de>
2025-10-07 19:24:56 -0400

    add docker support for frontend, fixed path for backend container, add vite proxy with dynamic url switch for docker or local host, moved define_views.sql to the right directory

--- milestone-1/frontend/vite.config.js
+++ milestone-1/frontend/vite.config.js
@@ -5,6 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+      host: '0.0.0.0',
+      proxy: {
+          '/api': {
+          target: process.env.VITE_BACKEND_URL || 'http://localhost:8000',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+          }
+      },
     port: 5173
   }
 })
